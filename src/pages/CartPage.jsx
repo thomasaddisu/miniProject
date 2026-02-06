d20f846402dbed55c9f752009a8ea3c708f1a472
thomas addisu <addisuthomas8@gmail.com>
2026-02-06 08:32:57 +0300

    make the remove button in cart page bellow quantity changer buttons for mobile view

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -16,43 +16,51 @@ function CartPage({ cartItems, increaseQty, decreaseQty, removeItem }) {
       <h1 className="text-2xl font-bold mb-6">Your Cart</h1>
       <div className="space-y-6">
         {cartItems.map((item) => (
-          <div
-            key={item.id}
-            className="flex items-center gap-4 border rounded-lg p-4 shadow-sm"
-          >
-            <img
-              src={item.image}
-              alt={item.name}
-              className="w-20 h-20 object-cover rounded"
-            />
-            <div className="flex-1">
-              <h2 className="font-semibold">{item.name}</h2>
-              <p className="text-green-600 font-bold">
-                ${item.price.toFixed(2)}
-              </p>
-            </div>
-            <div className="flex items-center gap-2">
-              <button
-                onClick={() => decreaseQty(item.id)}
-                className="w-6 h-6 bg-gray-200 rounded"
-              >
-                -
-              </button>
-              <span className="w-6 text-center">{item.quantity}</span>
-              <button
-                onClick={() => increaseQty(item.id)}
-                className="w-6 h-6 bg-gray-200 rounded"
-              >
-                +
-              </button>
-            </div>
-            <button
-              onClick={() => removeItem(item.id)}
-              className="ml-4 text-red-500 hover:underline"
-            >
-              Remove
-            </button>
-          </div>
+<div
+  key={item.id}
+  className="flex items-center gap-4 border rounded-lg p-4 shadow-sm"
+>
+  <img
+    src={item.image}
+    alt={item.name}
+    className="w-20 h-20 object-cover rounded"
+  />
+
+  <div className="flex-1">
+    <h2 className="font-semibold">{item.name}</h2>
+    <p className="text-green-600 font-bold">
+      ${item.price.toFixed(2)}
+    </p>
+  </div>
+
+
+  <div className="flex flex-col items-end sm:flex-row sm:items-center gap-2">
+  
+    <div className="flex items-center gap-2">
+      <button
+        onClick={() => decreaseQty(item.id)}
+        className="w-6 h-6 bg-gray-200 rounded"
+      >
+        -
+      </button>
+      <span className="w-6 text-center">{item.quantity}</span>
+      <button
+        onClick={() => increaseQty(item.id)}
+        className="w-6 h-6 bg-gray-200 rounded"
+      >
+        +
+      </button>
+    </div>
+
+    <button
+      onClick={() => removeItem(item.id)}
+      className="text-red-500 text-sm items-center mt-2 sm:mt-0 sm:ml-4 hover:underline"
+    >
+      Remove
+    </button>
+  </div>
+</div>
+
         ))}
       </div>
       <div className="mt-6 text-right text-xl font-bold">
